418e885d5b050d5b1a1e02ae28ceaed3305bf0a5
Jim Borden <jim.borden@couchbase.com>
2018-08-02 01:08:13 +0900

    Adds compressed message support, among other things (#6)
    * Adds initial compressed message support
    
    Also does the following
    1. Correct renamed header
    2. Add enum support for BLIP flags
    3. Add the message number to the summary text of the message
    4. Corrects the properties not being highlighted when selected in wireshark
    5. Adds checksum field
    
    * Update README
    
    Eliminate things done on this branch
    
    * Correct compression flaws from initial
    
    The caveat is that if frames are accessed randomly, the compression doesn’t work well

--- packet-blip.c
+++ packet-blip.c
@@ -9,7 +9,7 @@
 
 #include <epan/packet.h>
 #include <epan/tvbparse.h>
-#include <wsutil/wsjsmn.h>
+#include <wsutil/wsjson.h>
 
 #include <wsutil/str_util.h>
 #include <wsutil/unicode-utils.h>
@@ -17,6 +17,7 @@
 
 #include <wiretap/wtap.h>
 #include <stdio.h>
+#include <zlib.h>
 
 #include "packet-http.h"
 
@@ -74,6 +75,8 @@ static gboolean is_first_frame_in_msg(
         guint64 value_message_num
 );
 static int handle_ack_message(tvbuff_t*, packet_info*, proto_tree*, gint, guint64);
+static tvbuff_t* decompress(packet_info*, tvbuff_t*, gint, gint);
+static z_stream* get_decompress_stream(packet_info*);
 static dissector_handle_t blip_handle;
 
 // File level variables
@@ -84,8 +87,10 @@ static int hf_blip_properties_length = -1;
 static int hf_blip_properties = -1;
 static int hf_blip_message_body = -1;
 static int hf_blip_ack_size = -1;
+static int hf_blip_checksum = -1;
 static gint ett_blip = -1;
-
+static Bytef decompress_buffer[16384];
+static wmem_map_t* decompress_streams = NULL;
 
 static int
 dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
@@ -96,11 +101,9 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 
     /* Set the protcol column to say BLIP */
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "BLIP");
-
     /* Clear out stuff in the info column */
     col_clear(pinfo->cinfo,COL_INFO);
-
-
+    
     // ------------------------------------- Setup BLIP tree -----------------------------------------------------------
 
 
@@ -142,9 +145,10 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
             &value_frame_flags,
             ENC_VARINT_PROTOBUF);
 
-    printf("BLIP frame flags: %" G_GUINT64_FORMAT "\n", value_frame_flags);
+    guint64 masked = value_frame_flags & ~0x07;
+    printf("BLIP frame flags: %" G_GUINT64_FORMAT "\n", masked);
 
-    proto_tree_add_item(blip_tree, hf_blip_frame_flags, tvb, offset, varint_frame_flags_length, ENC_VARINT_PROTOBUF);
+    proto_tree_add_uint(blip_tree, hf_blip_frame_flags, tvb, offset, varint_frame_flags_length, (guint8)masked);
 
     offset += varint_frame_flags_length;
     printf("new offset: %d\n", offset);
@@ -153,23 +157,13 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
                    PRINTF_BINARY_PATTERN_INT8 "\n",
            PRINTF_BYTE_TO_BINARY_INT8(value_frame_flags));
 
-
-    // If it's compressed, don't try to do any more decoding
-    if (is_compressed(value_frame_flags) == TRUE) {
-        col_set_str(pinfo->cinfo, COL_INFO, "Compressed: BLIP dissector cannot decode further");
-        return tvb_captured_length(tvb);
-    }
-
-
     GString *msg_type = get_message_type(value_frame_flags);
+    g_string_append_printf(msg_type, "#%" G_GUINT64_FORMAT, value_message_num);
     col_add_str(pinfo->cinfo, COL_INFO, msg_type->str);
     g_string_free(msg_type, TRUE);
-
-
+    
     // If it's an ACK message, handle that separately, since there are no properties etc.
     if (is_ack_message(value_frame_flags) == TRUE) {
-        // col_set_str(pinfo->cinfo, COL_INFO, "ACK -- cannot decode further");
-        // return tvb_captured_length(tvb);
         return handle_ack_message(tvb, pinfo, blip_tree, offset, value_frame_flags);
     }
 
@@ -201,6 +195,25 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 
     // Update the conversation w/ the latest version of the blip_conversation_entry_t
     conversation_add_proto_data(conversation, proto_blip, (void *)conversation_entry_ptr);
+    
+    tvbuff_t* tvb_to_use = tvb;
+    gboolean compressed = is_compressed(value_frame_flags);
+    if(compressed) {
+        if(!tree) {
+            // This is just a preliminary pass, no protocol tree to fill in so
+            // don't waste time decompressing (it will probably mess things up
+            // anyway)
+            return tvb_reported_length(tvb);
+        }
+        
+        tvb_to_use = decompress(pinfo, tvb, offset, tvb_reported_length_remaining(tvb, offset) - BLIP_BODY_CHECKSUM_SIZE);
+        if(!tvb_to_use) {
+            proto_tree_add_string(blip_tree, hf_blip_message_body, tvb, offset, tvb_reported_length_remaining(tvb, offset), "<Error decompressing message>");
+            return tvb_reported_length(tvb);
+        }
+        
+        offset = 0;
+    }
 
     // Is this the first frame in a message?
     if (first_frame_in_msg == TRUE) {
@@ -212,7 +225,7 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 
         guint64 value_properties_length;
         guint value_properties_length_varint_length = tvb_get_varint(
-                tvb,
+                tvb_to_use,
                 offset,
                 FT_VARINT_MAX_LEN,
                 &value_properties_length,
@@ -220,7 +233,7 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 
         printf("BLIP properties length: %" G_GUINT64_FORMAT "\n", value_properties_length);
 
-        proto_tree_add_item(blip_tree, hf_blip_properties_length, tvb, offset, value_properties_length_varint_length, ENC_VARINT_PROTOBUF);
+        proto_tree_add_item(blip_tree, hf_blip_properties_length, tvb_to_use, offset, value_properties_length_varint_length, ENC_VARINT_PROTOBUF);
 
         offset += value_properties_length_varint_length;
         printf("new offset: %d\n", offset);
@@ -232,7 +245,7 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 
         // At this point, the length of the properties is known and is stored in value_properties_length.
         // This reads the entire properties out of the tvb and into a buffer (buf).
-        guint8* buf = tvb_get_string_enc(wmem_packet_scope(), tvb, offset, (gint) value_properties_length, ENC_UTF_8);
+        guint8* buf = tvb_get_string_enc(wmem_packet_scope(), tvb_to_use, offset, (gint) value_properties_length, ENC_UTF_8);
 
         // "Profile\0subChanges\0continuous\0true\0foo\0bar" -> "Profile:subChanges:continuous:true:foo:bar"
         // Iterate over buf and change all the \0 null characters to ':', since otherwise trying to set a header
@@ -245,44 +258,36 @@ dissect_blip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
                 }
             }
         }
-
-        // Since proto_tree_add_item() requires a tvbuff, convert the guint8* buf into a tvbuff.  tvb_new_child_real_data()
-        // is used so that it will be free'd at the same time that the parent tvb is freed.
-        // See WSDG 9.3. How to handle transformed data.
-        tvbuff_t* tvb_child = tvb_new_child_real_data(tvb, buf, (guint) value_properties_length, (guint) value_properties_length);
-
-        // Add this to the tree from the tvb_child tvbuff, and use offset=0 since it that buffer only
-        // contains the properties, which are now delimited by ':' in between each property.
-        // TODO: Since it's a child tvbuff, clicking this in the wireshark UI doesn't highlight the correct
-        // TODO: subset of the raw data.  That would be a nice-to-have
-        proto_tree_add_item(blip_tree, hf_blip_properties, tvb_child, 0, (guint) value_properties_length, ENC_UTF_8);
+        
+        if(value_properties_length > 0) {
+            proto_tree_add_string(blip_tree, hf_blip_properties, tvb_to_use, offset, (int)value_properties_length, (const char *)buf);
+        }
 
         // Bump the offset by the length of the properties
-        offset += (gint)gvalue_properties_length;
+        offset += (gint)value_properties_length;
         printf("new offset: %d\n", offset);
 
 
     }
 
-
-
     // ------------------------ BLIP Frame: Message Body --------------------------------------------------
 
     // WS_DLL_PUBLIC gint tvb_reported_length_remaining(const tvbuff_t *tvb, const gint offset);
-    gint reported_length_remaining = tvb_reported_length_remaining(tvb, offset);
+    gint reported_length_remaining = tvb_reported_length_remaining(tvb_to_use, offset);
 
     // Don't read in the trailing checksum at the end
-    if (reported_length_remaining >= BLIP_BODY_CHECKSUM_SIZE) {
+    if (!compressed && reported_length_remaining >= BLIP_BODY_CHECKSUM_SIZE) {
         reported_length_remaining -= BLIP_BODY_CHECKSUM_SIZE;
     }
-
-    proto_tree_add_item(blip_tree, hf_blip_message_body, tvb, offset, reported_length_remaining, ENC_UTF_8);
+    
+    if(reported_length_remaining > 0) {
+        proto_tree_add_item(blip_tree, hf_blip_message_body, tvb_to_use, offset, reported_length_remaining, ENC_UTF_8);
+    }
 
     offset += reported_length_remaining;
     printf("new offset: %d\n", offset);
-
-    // TODO: add the checksum to a separate UI field
-
+    
+    proto_tree_add_item(blip_tree, hf_blip_checksum, tvb, tvb_reported_length(tvb) - BLIP_BODY_CHECKSUM_SIZE, BLIP_BODY_CHECKSUM_SIZE, ENC_BIG_ENDIAN);
 
     // -------------------------------------------- Etc ----------------------------------------------------------------
 
@@ -399,6 +404,65 @@ handle_ack_message(tvbuff_t *tvb, packet_info *pinfo, proto_tree *blip_tree, gin
     return tvb_captured_length(tvb);
 }
 
+static gchar*
+message_hash_key_decompress(wmem_allocator_t* pool,
+                            packet_info *pinfo)
+{
+    // Derive the hash key to use
+    // srcport:destport
+    
+    gchar *srcport = g_strdup_printf("%u", pinfo->srcport);
+    gchar *dstport = g_strdup_printf("%u", pinfo->destport);
+    gchar *colon = g_strdup(":");
+    gchar *hash_key = wmem_strconcat(
+                                     pool,
+                                     srcport,
+                                     colon,
+                                     dstport,
+                                     NULL
+                                     );
+    
+    g_free(srcport);
+    g_free(dstport);
+    g_free(colon);
+    
+    return hash_key;
+}
+
+static gchar*
+message_hash_key_convo(wmem_allocator_t* pool,
+                 packet_info *pinfo,
+                 guint64 value_frame_flags,
+                 guint64 value_message_num)
+{
+    // Derive the hash key to use
+    // msgtype:srcport:destport:messagenum
+    
+    GString *msg_type = get_message_type(value_frame_flags);
+    gchar *srcport = g_strdup_printf("%u", pinfo->srcport);
+    gchar *dstport = g_strdup_printf("%u", pinfo->destport);
+    gchar *msgnum = g_strdup_printf("%lu", value_message_num);
+    gchar *colon = g_strdup(":");
+    gchar *hash_key = wmem_strconcat(
+                                     pool,
+                                     msg_type->str,
+                                     colon,
+                                     srcport,
+                                     colon,
+                                     dstport,
+                                     colon,
+                                     msgnum,
+                                     NULL
+                                     );
+    
+    g_free(srcport);
+    g_free(dstport);
+    g_free(msgnum);
+    g_free(colon);
+    
+    return hash_key;
+}
+
 // Finds out whether this is the first blip frame in the blip message (which can consist of a series of frames).
 // If it is, updates the conversation_entry_ptr->blip_requests hash to record the pinfo->num (wireshark packet number)
 static gboolean
@@ -411,26 +475,8 @@ is_first_frame_in_msg(blip_conversation_entry_t *conversation_entry_ptr, packet_
     wmem_allocator_t *pool;
     pool = wmem_allocator_new(WMEM_ALLOCATOR_SIMPLE);
 
-
-    // Derive the hash key to use
-    // msgtype:srcport:messagenum
-
-    GString *msg_type = get_message_type(value_frame_flags);
-    gchar *srcport = g_strdup_printf("%u", pinfo->srcport);
-    gchar *msgnum = g_strdup_printf("%lu", value_message_num);
-    gchar *colon = g_strdup(":");
-
     // TODO: this is a terrible memory leak .. it keeps creating new keys, never frees them
-    gchar *hash_key = wmem_strconcat(
-            pool,
-            msg_type->str,
-            colon,
-            srcport,
-            colon,
-            msgnum,
-            NULL
-    );
-
+    gchar *hash_key = message_hash_key_convo(pool, pinfo, value_frame_flags, value_message_num);
     guint* first_frame_number_for_msg = wmem_map_lookup(conversation_entry_ptr->blip_requests, (void *) hash_key);
 
     if (first_frame_number_for_msg != NULL) {
@@ -449,67 +495,155 @@ is_first_frame_in_msg(blip_conversation_entry_t *conversation_entry_ptr, packet_
         *frame_num_copy = pinfo->num;
 
         wmem_map_insert(conversation_entry_ptr->blip_requests, (void *) hash_key_copy, (void *) frame_num_copy);
-
     }
-
-    g_free(srcport);
-    g_free(msgnum);
-    g_free(colon);
 
     wmem_destroy_allocator(pool);  // destroy the temp memory pool
 
 
     return first_frame_in_msg;
-
-
 }
 
+static z_stream* get_decompress_stream(packet_info* pinfo)
+{
+    if(!decompress_streams) {
+        decompress_streams = wmem_map_new(wmem_file_scope(), g_str_hash, g_str_equal);
+    }
+    
+    wmem_allocator_t *pool;
+    pool = wmem_allocator_new(WMEM_ALLOCATOR_SIMPLE);
+    
+    gchar* hash_key = message_hash_key_decompress(pool, pinfo);
+    z_stream* decompress_stream = wmem_map_lookup(decompress_streams, (void *) hash_key);
+    if(decompress_stream) {
+        wmem_destroy_allocator(pool);
+        return decompress_stream;
+    }
+    
+    gchar *hash_key_copy = wmem_strdup(wmem_file_scope(), hash_key);
+    decompress_stream = wmem_alloc0(wmem_file_scope(), sizeof(z_stream));
+    wmem_map_insert(decompress_streams, hash_key_copy, decompress_stream);
+    wmem_destroy_allocator(pool);
+    return decompress_stream;
+}
 
+static tvbuff_t*
+decompress(packet_info* pinfo, tvbuff_t* tvb, gint offset, gint length)
+{
+    z_stream* decompress_stream = get_decompress_stream(pinfo);
+    static Byte trailer[4] = { 0x00, 0x00, 0xff, 0xff };
+    if(!decompress_stream->next_out) {
+        decompress_stream->zalloc = 0;
+        decompress_stream->zfree = 0;
+        decompress_stream->opaque = 0;
+        int err = inflateInit2(decompress_stream, -MAX_WBITS);
+        if(err != Z_OK) {
+            decompress_stream->next_out = 0;
+            REPORT_DISSECTOR_BUG("Unable to create INFLATE context to decompress messages");
+            return NULL;
+        }
+    }
+    
+    const guint8* buf = tvb_get_ptr(tvb, offset, length);
+    decompress_stream->next_in = (Bytef*)buf;
+    decompress_stream->avail_in = length;
+    decompress_stream->next_out = decompress_buffer;
+    decompress_stream->avail_out = 16384;
+    uLong start = decompress_stream->total_out;
+    int err = inflate(decompress_stream, Z_NO_FLUSH);
+    if(err < 0) {
+        printf("Error decompressing first step: %d\n", err);
+        return NULL;
+    }
+    
+    decompress_stream->next_in = trailer;
+    decompress_stream->avail_in = 4;
+    err = inflate(decompress_stream, Z_SYNC_FLUSH);
+    if(err < 0) {
+        printf("Error decompressing second step: %d\n", err);
+        return NULL;
+    }
+    
+    uLong bodyLength = decompress_stream->total_out - start;
+    guint8* poolBuffer = (guint8*)wmem_alloc(pinfo->pool, bodyLength);
+    memcpy(poolBuffer, decompress_buffer, bodyLength);
+    tvbuff_t* decompressedChild = tvb_new_real_data(poolBuffer, (guint)bodyLength, (gint)bodyLength);
+    add_new_data_source(pinfo, decompressedChild, "Decompressed Payload");
+    return decompressedChild;
+}
 
 void
 proto_register_blip(void)
 {
-
+    // Compressed = 0x08
+    // Urgent     = 0x10
+    // NoReply    = 0x20
+    // MoreComing = 0x40
+    // In ascending order so that a binary search will be used as per the
+    // README.dissector
+    static const value_string flag_combos[] = {
+        { 0x00, "None" },
+        { 0x08, "Compressed" },
+        { 0x10, "Urgent" },
+        { 0x20, "NoReply" },
+        { 0x40, "MoreComing" },
+        { 0x08|0x10, "Compressed|Urgent" },
+        { 0x08|0x20, "Compressed|NoReply" },
+        { 0x10|0x20, "Urgent|NoReply" },
+        { 0x08|0x40, "Compressed|MoreComing" },
+        { 0x10|0x40, "Urgent|MoreComing" },
+        { 0x20|0x40, "NoReply|MoreComing" },
+        { 0x08|0x10|0x20, "Compressed|Urgent|NoReply" },
+        { 0x08|0x10|0x40, "Compressed|Urgent|MoreComing" },
+        { 0x08|0x20|0x40, "Compressed|NoReply|MoreComing" },
+        { 0x10|0x20|0x40, "Urgent|NoReply|MoreComing" },
+        { 0x08|0x10|0x20|0x40, "Compressed|Urgent|NoReply|MoreComing" },
+        { 0, NULL }
+    };
+    static value_string_ext flag_combos_ext = VALUE_STRING_EXT_INIT(flag_combos);
+    
     static hf_register_info hf[] = {
             { &hf_blip_message_number,
-                    { "BLIP Message Number", "blip.messagenum",
+                    { "Message Number", "blip.messagenum",
                             FT_UINT64, BASE_DEC,
                             NULL, 0x0,
                             NULL, HFILL }
             },
             { &hf_blip_frame_flags,
-                    { "BLIP Frame Flags", "blip.frameflags",
-                            FT_UINT64, BASE_DEC,
-                            NULL, 0x0,
+                    { "Frame Flags", "blip.frameflags",
+                            FT_UINT8, BASE_HEX|BASE_EXT_STRING,
+                            &flag_combos_ext, 0x0,
                             NULL, HFILL }
             },
             { &hf_blip_properties_length,
-                    { "BLIP Properties Length", "blip.propslength",
+                    { "Properties Length", "blip.propslength",
                             FT_UINT64, BASE_DEC,
                             NULL, 0x0,
                             NULL, HFILL }
             },
             { &hf_blip_properties,
-                    { "BLIP Properties", "blip.props",
+                    { "Properties", "blip.props",
                             FT_STRING, STR_UNICODE,
                             NULL, 0x0,
                             NULL, HFILL }
             },
             { &hf_blip_message_body,
-                    { "BLIP Message Body", "blip.messagebody",
+                    { "Message Body", "blip.messagebody",
                             FT_STRING, STR_UNICODE,
                             NULL, 0x0,
                             NULL, HFILL }
             },
             { &hf_blip_ack_size,
-                    { "BLIP ACK num bytes", "blip.numackbytes",
+                    { "ACK num bytes", "blip.numackbytes",
                             FT_UINT64, BASE_DEC,
                             NULL, 0x0,
                             NULL, HFILL }
             },
-
-
-
+            { &hf_blip_checksum,
+                    { "Checksum", "blip.checksum",
+                        FT_UINT32, BASE_DEC,
+                        NULL, 0x0,
+                        NULL, HFILL }
+            }
     };
 
     /* Setup protocol subtree array */
@@ -523,7 +657,6 @@ proto_register_blip(void)
     proto_register_subtree_array(ett, array_length(ett));
 
     blip_handle = register_dissector("blip", dissect_blip, proto_blip);
-
 }
 
 void
@@ -548,6 +681,4 @@ proto_reg_handoff_blip(void)
     } else {
         // printf("not FT_STRING");
     }
-
-
 }
